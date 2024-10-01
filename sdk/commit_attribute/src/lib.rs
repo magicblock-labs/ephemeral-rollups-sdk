c09513cf96e2a393f38e74017d7354c6252a803d
Gabriele Picco <gabriele@magicblock.gg>
2024-10-01 11:20:46 +0700

    chore: Refactoring & Improve SDK

---
+++ sdk/commit_attribute/src/lib.rs
@@ -0,0 +1,55 @@
+extern crate proc_macro;
+use syn::parse::Parser;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Fields, Field, ItemStruct};
+
+#[proc_macro_attribute]
+pub fn commit(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as ItemStruct);
+
+    let name = &input.ident;
+    let expanded = if let Fields::Named(fields_named) = &input.fields {
+        let mut has_magic_program = false;
+        let mut has_magic_context = false;
+
+        for field in &fields_named.named {
+            if let Some(ident) = &field.ident {
+                if ident == "magic_program" {
+                    has_magic_program = true;
+                } else if ident == "magic_context" {
+                    has_magic_context = true;
+                }
+            }
+        }
+
+        let mut new_fields = fields_named.named.clone();
+
+        if !has_magic_program {
+            new_fields.push(Field::parse_named.parse2(quote! {
+                pub magic_program: Program<'info, MagicProgram>
+            }).unwrap());
+        }
+
+        if !has_magic_context {
+            new_fields.push(Field::parse_named.parse2(quote! {
+                #[account(mut, address = ephemeral_rollups_sdk::consts::MAGIC_CONTEXT_ID)]
+                /// CHECK:`
+                pub magic_context: AccountInfo<'info>
+            }).unwrap());
+        }
+
+        quote! {
+            #[derive(Accounts)]
+            pub struct #name<'info> {
+                #new_fields
+            }
+        }
+    } else {
+        quote! {
+            compile_error!("Commit attribute can only be used with structs with named fields");
+        }
+    };
+
+    TokenStream::from(expanded)
+}
--- sdk/src/anchor.rs
+++ sdk/src/anchor.rs
@@ -1,1 +1,31 @@
+#[cfg(feature = "anchor")]
 pub use ephemeral_rollups_sdk_attribute_delegate::delegate;
+
+#[cfg(feature = "anchor")]
+pub use ephemeral_rollups_sdk_attribute_commit::commit;
+
+#[cfg(feature = "anchor")]
+use solana_program::pubkey::Pubkey;
+
+#[cfg(feature = "anchor")]
+extern crate anchor_lang;
+
+#[cfg(feature = "anchor")]
+pub struct DelegationProgram;
+
+#[cfg(feature = "anchor")]
+impl anchor_lang::Id for DelegationProgram {
+    fn id() -> Pubkey {
+        crate::consts::DELEGATION_PROGRAM_ID
+    }
+}
+
+#[cfg(feature = "anchor")]
+pub struct MagicProgram;
+
+#[cfg(feature = "anchor")]
+impl anchor_lang::Id for MagicProgram {
+    fn id() -> Pubkey {
+        crate::consts::MAGIC_PROGRAM_ID
+    }
+}
