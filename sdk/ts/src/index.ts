672c4446b72469e255c1ef92bcc9db727886ffdc
Gabriele Picco <gabriele@magicblock.gg>
2024-10-11 15:23:28 +0700

    feat: Support delegation of on-curve accounts (#5)

--- sdk/ts/src/index.ts
+++ sdk/ts/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./seeds";
 export * from "./constants";
 export * from "./instructions/undelegate";
+export * from "./instructions/delegate";
 export * from "./accounts";
 export * from "./utils";
---
+++ sdk/ts/src/instructions/delegate.ts
@@ -0,0 +1,94 @@
+import * as beet from "@metaplex-foundation/beet";
+import * as web3 from "@solana/web3.js";
+import { PublicKey } from "@solana/web3.js";
+import { DELEGATION_PROGRAM_ID } from "../constants";
+import { DelegateAccounts } from "../accounts";
+
+export const delegateStruct = new beet.FixableBeetArgsStruct<{
+  instructionDiscriminator: number[];
+  valid_until: beet.bignum;
+  commit_frequency_ms: beet.bignum;
+  seeds: number[][];
+}>(
+  [
+    ["instructionDiscriminator", beet.uniformFixedSizeArray(beet.u8, 8)],
+    ["valid_until", beet.i64],
+    ["commit_frequency_ms", beet.u32],
+    ["seeds", beet.array(beet.array(beet.u8))],
+  ],
+  "UndelegateInstructionArgs",
+);
+
+export const delegateInstructionDiscriminator = [0, 0, 0, 0, 0, 0, 0, 0];
+
+// Define the DelegateAccountArgs structure
+interface DelegateAccountArgs {
+  valid_until: number;
+  commit_frequency_ms: number;
+  seeds: Uint8Array[][];
+}
+
+// Function to create a delegate instruction
+export function createDelegateInstruction(
+  accounts: {
+    payer: web3.PublicKey;
+    delegateAccount: web3.PublicKey;
+    ownerProgram: web3.PublicKey;
+    buffer?: web3.PublicKey;
+    delegationRecord?: web3.PublicKey;
+    delegationMetadata?: web3.PublicKey;
+    systemProgram?: web3.PublicKey;
+  },
+  args?: DelegateAccountArgs,
+  programId = new PublicKey(DELEGATION_PROGRAM_ID),
+) {
+  const { delegationRecord, delegationMetadata, bufferPda } = DelegateAccounts(
+    accounts.delegateAccount,
+    accounts.ownerProgram,
+  );
+
+  args = args ?? {
+    valid_until: 0,
+    commit_frequency_ms: 4294967295, // 2 ** 4 - 1,
+    seeds: [],
+  };
+
+  const keys: web3.AccountMeta[] = [
+    { pubkey: accounts.payer, isWritable: false, isSigner: true },
+    { pubkey: accounts.delegateAccount, isWritable: true, isSigner: true },
+    { pubkey: accounts.ownerProgram, isWritable: false, isSigner: false },
+    {
+      pubkey: accounts.buffer ?? bufferPda,
+      isWritable: true,
+      isSigner: false,
+    },
+    {
+      pubkey: accounts.delegationRecord ?? delegationRecord,
+      isWritable: true,
+      isSigner: false,
+    },
+    {
+      pubkey: accounts.delegationMetadata ?? delegationMetadata,
+      isWritable: true,
+      isSigner: false,
+    },
+    {
+      pubkey: accounts.systemProgram ?? web3.SystemProgram.programId,
+      isWritable: false,
+      isSigner: false,
+    },
+  ];
+
+  const [data] = delegateStruct.serialize({
+    instructionDiscriminator: delegateInstructionDiscriminator,
+    valid_until: args.valid_until,
+    commit_frequency_ms: args.commit_frequency_ms,
+    seeds: args.seeds.map((seed) => seed.map(Number)),
+  });
+
+  return new web3.TransactionInstruction({
+    programId,
+    keys,
+    data,
+  });
+}
