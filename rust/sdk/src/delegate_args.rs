2031c439ec36d6dec533e590677e370ee9935581
Babur Makhmudov <bmuddha13@gmail.com>
2025-02-04 12:19:16 +0400

    fixes

--- rust/sdk/src/delegate_args.rs
+++ rust/sdk/src/delegate_args.rs
@@ -67,15 +67,49 @@ impl From<DelegateAccounts> for DelegateAccountMetas {
 }
 
 impl DelegateAccountMetas {
-    pub fn into_vec(self, payer: AccountMeta) -> Vec<AccountMeta> {
+    pub fn into_vec(self, payer: Pubkey) -> Vec<AccountMeta> {
         vec![
-            payer,
+            AccountMeta::new(payer, true),
             self.delegated_account,
             self.owner_program,
             self.delegate_buffer,
             self.delegation_record,
             self.delegation_metadata,
             self.delegation_program,
+            self.system_program,
+        ]
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct UndelegateAccountMetas {
+    pub delegated_account: AccountMeta,
+    pub delegate_buffer: AccountMeta,
+    pub payer: AccountMeta,
+    pub system_program: AccountMeta,
+}
+
+impl UndelegateAccountMetas {
+    pub fn new(payer: Pubkey, delegated_account: Pubkey, owner_program: Pubkey) -> Self {
+        let delegate_buffer = delegate_buffer_pda_from_delegated_account_and_owner_program(
+            &delegated_account,
+            &owner_program,
+        );
+        Self {
+            delegated_account: AccountMeta::new(delegated_account, false),
+            delegate_buffer: AccountMeta::new_readonly(delegate_buffer, false),
+            payer: AccountMeta::new(payer, true),
+            system_program: AccountMeta::new_readonly(system_program::id(), false),
+        }
+    }
+}
+
+impl UndelegateAccountMetas {
+    pub fn into_vec(self) -> Vec<AccountMeta> {
+        vec![
+            self.delegated_account,
+            self.delegate_buffer,
+            self.payer,
             self.system_program,
         ]
     }
