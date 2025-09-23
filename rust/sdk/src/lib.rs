6884e1cd805767d3960467d1cf3702d4be50f777
taco-paco <edwinswatpako@gmail.com>
2025-09-23 12:35:28 +0400

    feat: update dlp using url. temporary

--- rust/sdk/src/lib.rs
+++ rust/sdk/src/lib.rs
@@ -11,6 +11,13 @@ pub mod utils;
 
 pub use dlp::args::{CallHandlerArgs, Context};
 pub use dlp::pda;
+pub use dlp::{
+    delegation_record_seeds_from_delegated_account, delegation_metadata_seeds_from_delegated_account,
+    commit_state_seeds_from_delegated_account, commit_record_seeds_from_delegated_account,
+    delegate_buffer_seeds_from_delegated_account, undelegate_buffer_seeds_from_delegated_account, fees_vault_seeds,
+    validator_fees_vault_seeds_from_validator, program_config_seeds_from_program_id,
+    ephemeral_balance_seeds_from_payer
+};
 pub use magicblock_magic_program_api::args::{
     ActionArgs, BaseActionArgs, CommitAndUndelegateArgs, CommitTypeArgs, MagicBaseIntentArgs,
     UndelegateTypeArgs,
