023691caf67fccec8708f8212a4a15ede8ac69bf
Dodecahedr0x <90185028+Dodecahedr0x@users.noreply.github.com>
2025-12-03 13:55:20 +0100

    feat: more errors for auth (#94)

--- ts/web3js/src/access-control/auth.ts
+++ ts/web3js/src/access-control/auth.ts
@@ -3,11 +3,12 @@ import { PublicKey } from "@solana/web3.js";
 export const SESSION_DURATION = 1000 * 60 * 60 * 24 * 30; // 30 days
 
 interface AuthChallengeResponse {
-  challenge: string;
+  challenge?: string;
+  error?: string;
 }
 
 interface AuthLoginResponse {
-  token: string;
+  token?: string;
   expiresAt?: number;
   error?: string;
 }
@@ -22,20 +23,28 @@ interface AuthLoginResponse {
 export async function getAuthToken(
   rpcUrl: string,
   publicKey: PublicKey,
-  signMessage: (message: Uint8Array) => Promise<Uint8Array>,
+  signMessage: (message: Uint8Array) => Promise<Uint8Array>
 ): Promise<{ token: string; expiresAt: number }> {
   // Import this way because bs58 is an ECMAScript module
   const bs58 = (await import("bs58")).default;
 
   // Getting the challenge from the RPC
   const challengeResponse = await fetch(
-    `${rpcUrl}/auth/challenge?pubkey=${publicKey.toString()}`,
+    `${rpcUrl}/auth/challenge?pubkey=${publicKey.toString()}`
   );
-  const { challenge }: AuthChallengeResponse = await challengeResponse.json();
+  const { challenge, error }: AuthChallengeResponse =
+    await challengeResponse.json();
+
+  if (error) {
+    throw new Error(`Failed to get challenge: ${error}`);
+  }
+  if (!challenge) {
+    throw new Error("No challenge received");
+  }
 
   // Signing the challenge
   const signature = await signMessage(
-    new Uint8Array(Buffer.from(challenge, "utf-8")),
+    new Uint8Array(Buffer.from(challenge, "utf-8"))
   );
   const signatureString = bs58.encode(signature);
 
@@ -53,6 +62,9 @@ export async function getAuthToken(
 
   if (authResponse.status !== 200) {
     throw new Error(`Failed to authenticate: ${authJson.error}`);
+  }
+  if (!authJson.token) {
+    throw new Error("No token received");
   }
 
   const expiresAt = authJson.expiresAt ?? Date.now() + SESSION_DURATION;
