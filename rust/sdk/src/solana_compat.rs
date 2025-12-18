36f94850e7763a1f02bf5ec36e1f2e8691dec8f8
Gabriele Picco <gabriele@magicblock.gg>
2025-12-18 09:54:59 +0100

    Fix: update resize function for conditional reallocation
    Refactor resize function to conditionally use realloc or resize based on feature flags.

--- rust/sdk/src/solana_compat.rs
+++ rust/sdk/src/solana_compat.rs
@@ -45,7 +45,16 @@ pub mod solana {
 
     #[inline(always)]
     pub fn resize(target_account: &AccountInfo, new_len: usize) -> ProgramResult {
-        target_account.resize(new_len)
+        #[cfg(not(feature = "disable-realloc"))]
+        {
+            #[allow(deprecated)]
+            target_account.realloc(new_len, false)
+        }
+
+        #[cfg(feature = "disable-realloc")]
+        {
+            target_account.resize(new_len)
+        }
     }
 }
 
