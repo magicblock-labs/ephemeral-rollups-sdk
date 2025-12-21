ec7ad20b5a6e2cab2b8fb8780e9a0f641555c993
Gabriele Picco <gabriele@magicblock.gg>
2025-12-21 18:24:09 +0100

    fix: memory violation on commit/undelegation (#95)
    * fix: memory violation on commit/undelegation
    
    * chore: refactor

--- rust/pinocchio/src/instruction/commit_and_undelegate.rs
+++ rust/pinocchio/src/instruction/commit_and_undelegate.rs
@@ -1,12 +1,5 @@
-use alloc::vec::Vec;
-use pinocchio::{
-    account_info::AccountInfo,
-    cpi::{slice_invoke, MAX_CPI_ACCOUNTS},
-    program_error::ProgramError,
-    ProgramResult,
-};
-
-use crate::utils::create_schedule_commit_ix;
+use crate::instruction::commit::commit_accounts_internal;
+use pinocchio::{account_info::AccountInfo, ProgramResult};
 
 pub fn commit_and_undelegate_accounts(
     payer: &AccountInfo,
@@ -14,19 +7,5 @@ pub fn commit_and_undelegate_accounts(
     magic_context: &AccountInfo,
     magic_program: &AccountInfo,
 ) -> ProgramResult {
-    let ix = create_schedule_commit_ix(payer, accounts, magic_context, magic_program, true)?;
-
-    let num_accounts = 1 + accounts.len(); // payer + accounts
-    if num_accounts > MAX_CPI_ACCOUNTS {
-        return Err(ProgramError::InvalidArgument);
-    }
-
-    let mut all_accounts: Vec<&AccountInfo> = Vec::with_capacity(accounts.len() + 1);
-    all_accounts.push(payer);
-    for account in accounts.iter() {
-        all_accounts.push(account);
-    }
-
-    slice_invoke(&ix, &all_accounts)?;
-    Ok(())
+    commit_accounts_internal(payer, accounts, magic_context, magic_program, true)
 }
