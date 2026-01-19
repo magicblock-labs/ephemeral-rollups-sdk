294b36b3ac33da171707d297e6dc0e4aea564267
jonasXchen <jonas002@hotmail.de>
2026-01-19 17:35:21 +0800

    feat: acl with pinocchio

--- rust/pinocchio/src/instruction/mod.rs
+++ rust/pinocchio/src/instruction/mod.rs
@@ -1,9 +1,21 @@
+pub mod close_permission;
 pub mod commit;
 pub mod commit_and_undelegate;
+pub mod commit_and_undelegate_permission;
+pub mod commit_permission;
+pub mod create_permission;
 pub mod delegate;
+pub mod delegate_permission;
 pub mod undelegate;
+pub mod update_permission;
 
+pub use close_permission::*;
 pub use commit::*;
 pub use commit_and_undelegate::*;
+pub use commit_and_undelegate_permission::*;
+pub use commit_permission::*;
+pub use create_permission::*;
 pub use delegate::*;
+pub use delegate_permission::*;
 pub use undelegate::*;
+pub use update_permission::*;
