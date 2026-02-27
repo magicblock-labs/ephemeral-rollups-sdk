0f68a5f17ca914514aae8b9b7161d9b41864d850
Dodecahedr0x <hexadecifish@gmail.com>
2026-02-27 16:30:26 +0100

    feat: enable PDA signed undelegation

--- rust/pinocchio/src/instruction/commit_and_undelegate.rs
+++ rust/pinocchio/src/instruction/commit_and_undelegate.rs
@@ -1,11 +1,19 @@
 use crate::instruction::commit::commit_accounts_internal;
-use pinocchio::{AccountView, ProgramResult};
+use pinocchio::{cpi::Signer, AccountView, ProgramResult};
 
 pub fn commit_and_undelegate_accounts(
     payer: &AccountView,
     accounts: &[AccountView],
     magic_context: &AccountView,
     magic_program: &AccountView,
+    signer_seeds: Option<&[Signer<'_, '_>]>,
 ) -> ProgramResult {
-    commit_accounts_internal(payer, accounts, magic_context, magic_program, true)
+    commit_accounts_internal(
+        payer,
+        accounts,
+        magic_context,
+        magic_program,
+        true,
+        signer_seeds,
+    )
 }
