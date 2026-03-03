c3c625145d24b15bd8aebabdd59b48300a430b9e
Sarfaraz Nawaz <sir_nawaz959@yahoo.com>
2026-03-03 23:43:01 +0530

    Add DelegateWithActions in pinocchio

--- rust/pinocchio/src/utils.rs
+++ rust/pinocchio/src/utils.rs
@@ -2,14 +2,21 @@ use crate::{
     consts::DELEGATION_PROGRAM_ID,
     types::{DelegateAccountArgs, MAX_DELEGATE_ACCOUNT_ARGS_SIZE},
 };
+use alloc::vec::Vec;
 use core::mem::MaybeUninit;
+use dlp::args::{DelegateArgs, DelegateWithActionsArgs, PostDelegationActions};
+use dlp::discriminator::DlpDiscriminator;
 use pinocchio::{
     address::MAX_SEEDS,
-    cpi::{invoke_signed, Seed, Signer, MAX_CPI_ACCOUNTS},
+    cpi::{
+        invoke_signed, invoke_signed_with_bounds, Seed, Signer, MAX_CPI_ACCOUNTS,
+        MAX_STATIC_CPI_ACCOUNTS,
+    },
     error::ProgramError,
     instruction::{InstructionAccount, InstructionView},
     AccountView,
 };
+use solana_program::pubkey::Pubkey;
 
 #[inline(always)]
 pub fn empty_seed<'a>() -> Seed<'a> {
@@ -167,6 +174,116 @@ fn cpi_delegate_with_discriminator(
     ];
 
     invoke_signed(&instruction, &acc_infos, &[signer_seeds])?;
+    Ok(())
+}
+
+#[allow(clippy::too_many_arguments)]
+pub fn cpi_delegate_with_actions(
+    payer: &AccountView,
+    pda_acc: &AccountView,
+    owner_program: &AccountView,
+    buffer_acc: &AccountView,
+    delegation_record: &AccountView,
+    delegation_metadata: &AccountView,
+    system_program: &AccountView,
+    delegate_args: DelegateAccountArgs,
+    actions: PostDelegationActions,
+    action_signer_accounts: &[&AccountView],
+    signer_seeds: Signer<'_, '_>,
+) -> Result<(), ProgramError> {
+    if action_signer_accounts.len() != actions.signers.len() {
+        return Err(ProgramError::NotEnoughAccountKeys);
+    }
+
+    const UNINIT_ACCOUNT: MaybeUninit<InstructionAccount> =
+        MaybeUninit::<InstructionAccount>::uninit();
+    let mut account_metas = [UNINIT_ACCOUNT; MAX_CPI_ACCOUNTS];
+
+    let num_accounts = 7 + action_signer_accounts.len();
+    if num_accounts > MAX_CPI_ACCOUNTS {
+        return Err(ProgramError::InvalidArgument);
+    }
+
+    unsafe {
+        account_metas
+            .get_unchecked_mut(0)
+            .write(InstructionAccount::writable_signer(payer.address()));
+        account_metas
+            .get_unchecked_mut(1)
+            .write(InstructionAccount::writable_signer(pda_acc.address()));
+        account_metas
+            .get_unchecked_mut(2)
+            .write(InstructionAccount::readonly(owner_program.address()));
+        account_metas
+            .get_unchecked_mut(3)
+            .write(InstructionAccount::writable(buffer_acc.address()));
+        account_metas
+            .get_unchecked_mut(4)
+            .write(InstructionAccount::writable(delegation_record.address()));
+        account_metas
+            .get_unchecked_mut(5)
+            .write(InstructionAccount::writable(delegation_metadata.address()));
+        account_metas
+            .get_unchecked_mut(6)
+            .write(InstructionAccount::readonly(&pinocchio_system::ID));
+    }
+
+    let mut i = 0;
+    while i < action_signer_accounts.len() {
+        unsafe {
+            account_metas
+                .get_unchecked_mut(7 + i)
+                .write(InstructionAccount::readonly_signer(
+                    action_signer_accounts[i].address(),
+                ));
+        }
+        i += 1;
+    }
+
+    let seeds_vec: Vec<Vec<u8>> = delegate_args.seeds.iter().map(|s| s.to_vec()).collect();
+    let delegate = DelegateArgs {
+        commit_frequency_ms: delegate_args.commit_frequency_ms,
+        seeds: seeds_vec,
+        validator: delegate_args
+            .validator
+            .map(|v| Pubkey::new_from_array(*v.as_array())),
+    };
+    let args = DelegateWithActionsArgs { delegate, actions };
+
+    let mut data = DlpDiscriminator::DelegateWithActions.to_vec();
+    let payload = bincode1::serialize(&args).map_err(|_| ProgramError::InvalidInstructionData)?;
+    data.extend_from_slice(&payload);
+
+    let instruction = InstructionView {
+        program_id: &DELEGATION_PROGRAM_ID,
+        accounts: unsafe {
+            core::slice::from_raw_parts(
+                account_metas.as_ptr() as *const InstructionAccount,
+                num_accounts,
+            )
+        },
+        data: &data,
+    };
+
+    let mut acc_infos: [&AccountView; MAX_STATIC_CPI_ACCOUNTS] = [payer; MAX_STATIC_CPI_ACCOUNTS];
+    acc_infos[0] = payer;
+    acc_infos[1] = pda_acc;
+    acc_infos[2] = owner_program;
+    acc_infos[3] = buffer_acc;
+    acc_infos[4] = delegation_record;
+    acc_infos[5] = delegation_metadata;
+    acc_infos[6] = system_program;
+    let mut j = 0;
+    while j < action_signer_accounts.len() {
+        acc_infos[7 + j] = action_signer_accounts[j];
+        j += 1;
+    }
+
+    invoke_signed_with_bounds::<MAX_STATIC_CPI_ACCOUNTS>(
+        &instruction,
+        &acc_infos[..num_accounts],
+        &[signer_seeds],
+    )?;
     Ok(())
 }
 
