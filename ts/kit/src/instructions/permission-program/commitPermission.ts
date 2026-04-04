2c638c156899e6150d833605faf0b9163ea18b6d
cryptam <102138190+CryptAm@users.noreply.github.com>
2026-04-04 11:31:41 +0500

    Update commitPermission.ts

--- ts/kit/src/instructions/permission-program/commitPermission.ts
+++ ts/kit/src/instructions/permission-program/commitPermission.ts
@@ -33,7 +33,7 @@ export async function createCommitPermissionInstruction(accounts: {
     {
       address: accounts.authority[0],
       role: accounts.authority[1]
-        ? AccountRole.WRITABLE_SIGNER
+        ? AccountRole.READONLY_SIGNER
         : AccountRole.READONLY,
     },
     {
