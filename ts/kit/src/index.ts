2bc4da207ced1d9e6adf0f26fe9c17d24c0a060a
Dodecahedr0x <hexadecifish@gmail.com>
2026-04-20 14:57:09 +0200

    style: lint

--- ts/kit/src/index.ts
+++ ts/kit/src/index.ts
@@ -3,5 +3,6 @@ export * from "./pda.js";
 export * from "./utils.js";
 export * from "./resolver.js";
 export * from "./connection.js";
-export * from "./confirmation.js"; export * from "./instructions/index.js";
+export * from "./confirmation.js";
+export * from "./instructions/index.js";
 export * from "./access-control/index.js";
