d45ced8917b8f35bf5f7c46d62f3cf03e844e42b
Dodecahedr0x <hexadecifish@gmail.com>
2026-05-06 17:28:34 +0200

    test: data buffer size

--- rust/pinocchio/src/acl/instruction/mod.rs
+++ rust/pinocchio/src/acl/instruction/mod.rs
@@ -23,3 +23,49 @@ use crate::acl::MAX_MEMBER_SIZE;
 pub const fn data_buffer_size(n: usize) -> usize {
     8 + 1 + n * MAX_MEMBER_SIZE
 }
+
+#[cfg(test)]
+mod tests {
+    use pinocchio::Address;
+
+    use crate::acl::{EphemeralMembersArgs, Member, MemberFlags};
+
+    use super::*;
+
+    #[test]
+    fn test_data_buffer_size() {
+        // Substract discriminator
+        let mut bytes = [0; data_buffer_size(0) - 8];
+        let actual_len = EphemeralMembersArgs {
+            is_private: false,
+            members: &[],
+        }
+        .to_bytes(&mut bytes)
+        .unwrap();
+        assert_eq!(bytes.len(), actual_len);
+
+        let mut bytes = [0; data_buffer_size(1) - 8];
+        let actual_len = EphemeralMembersArgs {
+            is_private: false,
+            members: &[Member {
+                flags: MemberFlags::default(),
+                pubkey: Address::new_from_array([0; 32]),
+            }],
+        }
+        .to_bytes(&mut bytes)
+        .unwrap();
+        assert_eq!(bytes.len(), actual_len);
+
+        let mut bytes = [0; data_buffer_size(100) - 8];
+        let actual_len = EphemeralMembersArgs {
+            is_private: false,
+            members: &core::array::from_fn::<_, 100, _>(|_| Member {
+                flags: MemberFlags::default(),
+                pubkey: Address::new_from_array([0; 32]),
+            }),
+        }
+        .to_bytes(&mut bytes)
+        .unwrap();
+        assert_eq!(bytes.len(), actual_len);
+    }
+}
